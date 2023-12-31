61ff77f8905e4039eeb115ac8276398e5d8979fc
DominicArthur <g00407252@ATU.ie>
2023-12-31 15:06:47 +0000

    Added better coding practices by splliting the code into html and js for better mangemnt.

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-
 const express = require('express');
 const app = express();
-// The port number for the server to listen on
-const port = 3000; 
+const path = require('path');
+const port = 3000;// The port number for the server to listen on
+app.use(express.static('public'));
 
-// Define the route for the Home Page
+
 app.get('/', (req, res) => {
-  // HTML response with links to other pages
-  res.send(`
-    <h1>Welcome to the Home Page</h1>
-    <ul>
-      <li><a href="/stores">Stores Page</a></li>
-      <li><a href="/products">Products Page</a></li>
-      <li><a href="/managers">Managers (MongoDB) Page</a></li>
-    </ul>
-  `);
-});
-
-// Define the route for the Stores Page
-app.get('/stores', (req, res) => {
-  
-  res.send('<h1>Stores Page</h1>');
-});
-
-// Define the route for the Products Page
-app.get('/products', (req, res) => {
-  
-  res.send('<h1>Products Page</h1>');
-});
-
-// Define the route for the Managers (MongoDB) Page
-app.get('/managers', (req, res) => {
-  
-  res.send('<h1>Managers (MongoDB) Page</h1>');
+  // Send the 'index.html' file when a user accesses the root URL
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+  // Log a message when the server starts
+  console.log(`Server listening at http://localhost:${port}`);
 });
+
+
 
